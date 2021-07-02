9ba50455e980a9463de6859cee86edf6d57adef7
techtheriac <jezreel.franklin@decagon.dev>
2021-07-02 14:00:51 +0100

    Add GradingApp logic

--- GPACalculator/GradingApp.cs
+++ GPACalculator/GradingApp.cs
@@ -6,5 +6,33 @@ namespace GPACalculator
 {
     class GradingApp
     {
+        List<Course> CourseList = new List<Course> { };
+
+        private int GetWeightPointTotal ()
+        {
+            int total = 0;
+
+            foreach (var item in CourseList)
+            {
+                total += item.WeightPoint;
+            }
+
+            return total;
+        }
+
+        private int GetGradeUnitTotal ()
+        {
+            int total = 0;
+
+            foreach (var item in CourseList)
+            {
+                total += item.GradeUnit;
+            }
+
+            return total;
+        }
+
+        public int TotalGradeUnit => GetGradeUnitTotal();
+        public int TotalWeightPoint => GetWeightPointTotal();
     }
 }
